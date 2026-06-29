9a3196e773e4008305293b6b9ef905c9931c7bf5
mohebul123 <mohebulhassan123@gmail.com>
2026-06-29 17:21:19 +0600

    Chore: Fixes some problem for deployment

--- config/database.go
+++ config/database.go
@@ -15,19 +15,23 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// Load .env file
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, reading from system environment")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_SSLMODE"),
-	)
+	dsn := os.Getenv("DB_URL")
+
+	if dsn == "" {
+		log.Println("DB_URL not found, constructing DSN from individual DB variables...")
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_SSLMODE"),
+		)
+	}
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -44,7 +48,7 @@ func ConnectDatabase() {
 	sqlDB.SetConnMaxLifetime(time.Hour * 1)
 
 	DB = database
-	// Auto Migrate the Models
+
 	err = database.AutoMigrate(&models.User{}, &models.ParkingZone{}, &models.Reservation{})
 	if err != nil {
 		log.Fatal("Failed to run database migration:", err)
